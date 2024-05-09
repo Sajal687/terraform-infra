e853d510da830d75841e3353d9f633aa488e835b
Sajal687 <sajalnema781@gmail.com>
2024-05-09 15:58:02 +0530

    Adding IAM Infra

---
+++ infra/IAM/IAMUserGroups.tf
@@ -0,0 +1,34 @@
+resource "aws_iam_user_membership" "MultiGroups" {
+    user = aws_iam_user_U1.name
+
+    groups = [
+        aws_iam_group.G1.name,
+        aws_iam_group.G2.name,
+    ]
+} 
+
+resource "aws_iam_user_membership" "SingleGroups" {
+    user = aws_iam_user_U1.name
+
+    groups = [
+        aws_iam_group.G3.name,
+    ]
+} 
+
+resource "aws_iam_user" "U1" {
+    name = "user1"  
+}
+
+
+resource "aws_iam_group" "G1" {
+  name = "group1"
+}
+
+
+resource "aws_iam_group" "G2" {
+  name = "group2"
+}
+
+resource "aws_iam_group" "G3" {
+  name = "group3"
+}
---
+++ infra/IAM/provider.tf
@@ -0,0 +1,5 @@
+provider "random" {}
+
+provider "aws" {
+    region = "ca-central-1"
+}
