31fa761e437be12943a34b08398049abf84e5476
Sajal687 <sajalnema781@gmail.com>
2024-05-09 16:52:07 +0530

    Update

--- infra/IAM/provider.tf
+++ infra/IAM/provider.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-sn-file"
+    key = "key/terraform.tfstate"
+    region = "ca-central-1"
+  }
+}
+
 provider "random" {}
 
 provider "aws" {
