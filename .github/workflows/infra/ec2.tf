3f1d31f9d76058d9459aa6144b5c9a8b1504fb67
Sajal687 <sajalnema781@gmail.com>
2024-05-09 12:38:54 +0530

    First Commit

---
+++ .github/workflows/infra/ec2.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+    region = ap-south-1  
+}
+
+resource "aws_instance" "my_instance" {
+      ami = "ami-07117708253546063"
+      instance_type = "t2.micro"
+
+      tags = {
+        Name: "var.ec2_name"
+      }
+}
